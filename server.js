b556f470c626723806706c983bb10673340e9d49
Maria Emil <maria.emil@example.com>
2026-04-17 19:09:52 +0200

    testing

--- server.js
+++ server.js
@@ -94,6 +94,7 @@ app.use((req, res, next) => {
     next();
 });
 
+
 app.use(express.static(__dirname + '/public'));// serves your HTML/CSS/JS files
 
 // ============================================
